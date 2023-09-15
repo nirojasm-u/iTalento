d0c9608962810c229558956832aac939d7f6a74b
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-09-14 19:35:16 -0500

    anterior

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+//Settings
+app.set('port',3003);
+
+//Middlewares
+
+//Rutas
+
+//Static files (public)
+app.use(express.static(__dirname + "/public"));
 
 //Puerto para correr
-app.listen(3000, () => {
-	console.log("Server on port http://localhost:3000");
+app.listen(app.get('port'), () => {
+	console.log("Server on port http://localhost:" + app.get('port'));
 });
