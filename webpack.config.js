54235ffb09cde42174d8cbc139f87312948b92d8
leoleguizamon <anlleguizamongu@unal.edu.co>
2023-09-14 19:38:09 -0500

    inicializacion vue

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+	entry: './src/app/index.js',
+	output: {
+		path: __dirname + '/src/public',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/, // Busca todos los archivos con extensión .js
+				exclude: /node_modules/, // Excluye la carpeta node_modules
+				use: {
+					loader: 'babel-loader' // Utiliza el loader de babel
+				}
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'// Utiliza el loader de vue
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin()
+	]
+
+};
