0c591de33277cf03751de1a37a1f6591f88ea193
nirojasm-u <nirojasm@unal.edu.co>
2023-09-19 12:06:51 -0500

    Auth0 Middleware

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const morgan = require('morgan');
 const mysql = require('mysql2');
 const app = express();
+const { auth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASEURL,
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUER
+};
 
 //Settings
-app.set('port', 3003);
+app.set('port', 3000);
 
 //Middlewares
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 //Rutas (Usa el router de express)
 app.use('/',require('./controllers/router'));
