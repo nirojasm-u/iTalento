d8c19f347e7a323a550ac53ab6685e00f042fa8c
nirojasm-u <nirojasm@unal.edu.co>
2023-10-05 16:57:12 -0500

    Db Fix, Ahora con GCloud

--- src/controllers/db.js
+++ src/controllers/db.js
@@ -3,7 +3,12 @@ require('dotenv').config()
 
 const mysql = require("mysql2");
 
-const conexion = mysql.createConnection(process.env.DATABASE_URL);
+const conexion = mysql.createConnection({
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE
+});
 
 conexion.connect((error) => {
 	if (error) {
